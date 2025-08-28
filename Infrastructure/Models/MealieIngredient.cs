debc82dede0bdc9853a1ef550b1496ff3781b260
Adam Rodriguez <rodrap50@gmail.com>
2025-08-28 11:00:10 -0700

    Set Unit to dictionary

--- Infrastructure/Models/MealieIngredient.cs
+++ Infrastructure/Models/MealieIngredient.cs
@@ -4,6 +4,6 @@ public class MealieIngredient
 {
     public MealieFood Food { get; set; } = new();
     public double Quantity { get; set; }
-    public string? Unit { get; set; }
+    public Dictionary<string, object> Unit { get; set; } = new();
     public string? Note { get; set; }
 }
