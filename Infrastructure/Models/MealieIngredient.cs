6f8e1304a5657515299bf3ccf7464141c4f9b69f
Adam Rodriguez <rodrap50@gmail.com>
2025-08-27 19:39:21 -0700

    Try double

--- Infrastructure/Models/MealieIngredient.cs
+++ Infrastructure/Models/MealieIngredient.cs
@@ -3,7 +3,7 @@
 public class MealieIngredient
 {
     public MealieFood Food { get; set; } = new();
-    public int Quantity { get; set; }
+    public double Quantity { get; set; }
     public string? Unit { get; set; }
     public string? Note { get; set; }
 }
